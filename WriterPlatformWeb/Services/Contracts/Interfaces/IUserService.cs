37bfceec6a1e90954453c99ab4561fd9dc306eb5
thisfozz <kfcfozz@vk.com>
2024-08-28 23:55:59 +0500

    Дописал нужные сервисы

--- WriterPlatformWeb/Services/Contracts/Interfaces/IUserService.cs
+++ WriterPlatformWeb/Services/Contracts/Interfaces/IUserService.cs
@@ -1,6 +1,15 @@
-﻿namespace WriterPlatformWeb.Services.Contracts.Interfaces;
+﻿using DataAccess.Entities;
+using WriterPlatformWeb.Models.Work;
+
+namespace WriterPlatformWeb.Services.Contracts.Interfaces;
 
 public interface IUserService
 {
-
+    Task<UserDTO> RegisterUserAsync(string login, string password, string email, int roleId);
+    Task<UserDTO?> AuthenticateUserAsync(string login, string password);
+    Task<bool> DeleteUserAsync(int userId);
+    Task<UserDTO?> GetUserByIdAsync(int userId);
+    Task<UserDTO?> GetUserByLoginAsync(string login);
+    Task<bool> UpdateUserAsync(int userId, string newEmail, string newPassword);
+    Task<bool> UpdateUserRoleAsync(string login, int newRoleId);
 }
--- WriterPlatformWeb/Services/Contracts/Interfaces/IWorkService.cs
+++ WriterPlatformWeb/Services/Contracts/Interfaces/IWorkService.cs
@@ -1,6 +1,16 @@
-﻿namespace WriterPlatformWeb.Services.Contracts.Interfaces;
+﻿using WriterPlatformWeb.Models.Work;
+
+namespace WriterPlatformWeb.Services.Contracts.Interfaces;
 
 public interface IWorkService
 {
-
+    Task<WorkDTO> PublishWorkAsync(string title, int genreId, int authorId, DateOnly publicationDate, string text);
+    Task<WorkDTO?> GetWorkByIdAsync(int workId);
+    Task<List<WorkDTO>> SearchWorksAsync(string authorName, string title, int genreId);
+    Task<List<WorkDTO>> GetTopWorksByRatingAsync(int topCount = 50);
+    Task<List<WorkDTO>> GetTopWorksByCommentsAsync(int topCount = 50);
+    Task<decimal?> GetCurrentRatingAsync(int workId);
+    Task<bool> UpdateRatingAsync(int workId, decimal newAverageRating);
+    Task<CommentDTO> AddCommentAsync(int userId, int workId, string text);
+    Task<bool> DeleteCommentAsync(int commentId);
 }
