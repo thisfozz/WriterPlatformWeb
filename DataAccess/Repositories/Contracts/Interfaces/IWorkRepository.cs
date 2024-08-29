709280f3d73840ab17975420c4fe5ed310559ec0
thisfozz <kfcfozz@vk.com>
2024-08-30 00:18:53 +0500

    Контроллер по работе с работами

--- DataAccess/Repositories/Contracts/Interfaces/IWorkRepository.cs
+++ DataAccess/Repositories/Contracts/Interfaces/IWorkRepository.cs
@@ -4,9 +4,10 @@ namespace DataAccess.Repositories.Contracts.Interfaces;
 
 public interface IWorkRepository
 {
+    Task<IEnumerable<WorkEntity>> GetAllWorksAsync();
     Task<WorkEntity> PublishWorkAsync(string title, int genreId, int authorId, DateOnly publicationDate, string text);
     Task<WorkEntity?> GetWorkByIdAsync(int workId);
-    Task<IEnumerable<WorkEntity>> SearchWorksAsync(string authorName, string title, int genreId);
+    Task<IEnumerable<WorkEntity>> SearchWorksAsync(string value);
     Task<IEnumerable<WorkEntity>> GetTopWorksByRatingAsync(int topCount = 50);
     Task<IEnumerable<WorkEntity>> GetTopWorksByCommentsAsync(int topCount = 50);
     Task<decimal?> GetCurrentRatingAsync(int workId);
